f2f809f999fa3aa56d3c8e34d14c20cdbe2025ed
Takekazu Omi <takekazuomi@gmail.com>
2019-01-02 09:29:57 +0900

    cleanup

--- psakefile.ps1
+++ psakefile.ps1
@@ -5,12 +5,13 @@ Task default -Depends CreateNewEnv
 
 Properties {
     $name = "myenv"
+    $packages = @("scikit-learn", "python=3.7", "pandas", "jupyter", "keyring", "matplotlib", "joblib", "pyodbc", "sqlalchemy")
 }
 
 
 Task CreateNewEnv {
-    Exec {conda create -y -n $name scikit-learn=0.20 python=3.7 pandas jupyter keyring matplotlib joblib pyodbc sqlalchemy }
-    Exec {activate.ps1 $name}
+    Exec {conda create -y -n $name $packages }
+    activate.ps1 $name
     conda config --env --append channels conda-forge
     conda install -y jupyter_contrib_nbextensions
     pip install lightgbm
@@ -20,3 +21,4 @@ Task CreateNewEnv {
 Task CleanEnv {
     conda info -e --json | convertfrom-json | Select-Object -ExpandProperty Envs | split-path -Leaf | ?{ $_ -like "$name*"} | %{conda remove -n $_ --all}
 }
+
